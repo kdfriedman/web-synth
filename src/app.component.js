37c0282a09da966310c1bd920c45377cc998f91d
kfriedman <kfriedman@H10897.ad.hugeinc.com>
2021-04-13 13:49:50 -0400

    feat(remove envelope section)

--- src/app.component.js
+++ src/app.component.js
@@ -1,51 +1,50 @@
-
-import { OscillatorComponent } from './oscillator.component';
-import { EnvelopeComponent } from './envelope.component';
-import { ControlBoardComponent } from './control-board.component';
-import { keyAmount } from './config';
-import { keyLetterDictionary, waveFormTypes, envelopeTemplate } from './const.js';
-import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
+import { OscillatorComponent } from "./oscillator.component";
+import { ControlBoardComponent } from "./control-board.component";
+import { keyAmount } from "./config";
+import {
+  keyLetterDictionary,
+  waveFormTypes,
+  envelopeTemplate,
+} from "./const.js";
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
 class AppComponent {
-    constructor() {
-        this.dom = dom;
-        this.library = library;
-        this.fas = fas;
-    }
+  constructor() {
+    this.dom = dom;
+    this.library = library;
+    this.fas = fas;
+  }
 
-    init() {
-        this.bootstrapComponents();
-        this.convertSVGElementsOnLoad();
-    }
+  init() {
+    this.bootstrapComponents();
+    this.convertSVGElementsOnLoad();
+  }
 
-    bootstrapComponents() {
-        const renderOptions = {
-            keyAmount: keyAmount,
-            keyLetterDictionary: keyLetterDictionary,
-            waveFormTypes: waveFormTypes,
-            envelopeTemplate: envelopeTemplate,
-        };
+  bootstrapComponents() {
+    const renderOptions = {
+      keyAmount: keyAmount,
+      keyLetterDictionary: keyLetterDictionary,
+      waveFormTypes: waveFormTypes,
+      envelopeTemplate: envelopeTemplate,
+    };
 
-        const controlBoard = new ControlBoardComponent(renderOptions);
-        controlBoard.renderKeyboard();
+    const controlBoard = new ControlBoardComponent(renderOptions);
+    controlBoard.renderKeyboard();
 
-        const oscillator = new OscillatorComponent();
-        oscillator.wireUpSynth();
+    const oscillator = new OscillatorComponent();
+    oscillator.wireUpSynth();
 
-        const envelope = new EnvelopeComponent(renderOptions);
-        envelope.renderEnvelope();
+    //getter returns keyboard component html container to use as parent element
+    controlBoard.retrieveHtmlContainerElem.children[0].children[0].children[0].appendChild(
+      envelope.retrieveHtmlContainerElem
+    );
+  }
 
-        //getter returns keyboard component html container to use as parent element
-        controlBoard.retrieveHtmlContainerElem.children[0].children[0].children[0]
-        .appendChild(envelope.retrieveHtmlContainerElem);
-    }
-
-    convertSVGElementsOnLoad() {
-        this.library.add(this.fas); 
-        this.dom.i2svg()
-        .then(svgElemLoaded => console.log('svgs done'));
-    }
+  convertSVGElementsOnLoad() {
+    this.library.add(this.fas);
+    this.dom.i2svg().then((svgElemLoaded) => console.log("svgs done"));
+  }
 }
 
 const App = new AppComponent();
